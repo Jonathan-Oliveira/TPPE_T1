e85575475067afcf1019005f139802eccfd281d3
Wellington Jonathan <wellpriz@gmail.com>
2022-12-21 20:41:08 -0300

    cadastro  rendimentos falsificacao

--- src/rendimento.py
+++ src/rendimento.py
@@ -2,3 +2,12 @@ class Rendimento:
     def __init__(self, descricao, valor)->None:
         self.descricao = descricao
         self.valor = valor
+
+class DescricaoEmBrancoException(Exception):
+    
+    def __init__(self, descricao: str) -> None:
+        self.descricao = descricao
+        super().__init__(f"Descrição em branco: {descricao}")
+
+    def __str__(self) -> str:
+        return f"Descrição em branco: {self.descricao}"      
--- test/test_rendimentos.py
+++ test/test_rendimentos.py
@@ -1,21 +1,39 @@
-from rendimento import Rendimento
+from rendimento import Rendimento, DescricaoEmBrancoException
 
 class TestRendimentos:
 
-    def test_cadastro(self):
-        rendimento = Rendimento(descricao = "Salario", valor =1200)
-        assert rendimento.descricao == "Salario"
-        assert rendimento.valor == 1200
 
-    def test_cadastro2(self):
-        rendimento = rendimento(descricao="aluguel", valor=800)
-        assert rendimento.valor == 800
-        assert rendimento.descricao == "aluguel"
-   
-    def test_cadastro3(self):
-        rendimento = rendimento(descricao="ações", valor=1000)
-        assert rendimento.valor == 800
-        assert rendimento.descricao == "ações"
+    @pytest.mark.parametrize(
+            "test_input, expected",
+            [
+                (
+                    {"descricao": "Salario", "valor": 1200},
+                    {"descricao": "Salario", "valor": 1200},
+                ),
+                (
+                    {"descricao": "aluguel", "valor": 800},
+                    {"descricao": "aluguel", "valor": 800},
+                ),
+                (
+                    {"descricao": "ações", "valor": 1000},
+                    {"descricao": "ações", "valor": 1000},
+                ),
+                # pytest.param("6*9", 42, marks=pytest.mark.xfail),
+            ],
+        )
+
+
+    def test_cadastro(self, test_input, expected):
+        rendimento = Rendimento(
+            descricao=test_input.get("descricao"),
+            valor=test_input.get("valor"),
+        )
+        assert rendimento.valor == expected.get("valor")
+        assert rendimento.descricao == expected.get("descricao")
+
+    def test_descricao_em_branco(self):
+        with pytest.raises(DescricaoEmBrancoException):
+            Rendimento(descricao="", valor=1200)
     
 
 
