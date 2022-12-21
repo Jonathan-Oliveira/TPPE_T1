032781f77c94a3a5402d494ca9e530d04ee99773
Wellington Jonathan <wellpriz@gmail.com>
2022-12-21 20:20:19 -0300

    cadastro  rendimentos duplicação

--- test/test_rendimentos.py
+++ test/test_rendimentos.py
@@ -1,9 +1,16 @@
 from rendimento import Rendimento
 
 class TestRendimentos:
+    
     def test_cadastro(self):
-        rendimento = Rendimento(descricao = "Salario",valor =1200)
+        rendimento = Rendimento(descricao = "Salario", valor =1200)
         assert rendimento.descricao == "Salario"
         assert rendimento.valor == 1200
+
+     def test_cadastro2(self):
+        rendimento = rendimento(descricao="aluguel", valor=800)
+        assert rendimento.valor == 800
+        assert rendimento.descricao == "aluguel"
+    
 
 
