e8d314fa5152c176086798c93b3b670c3b0a87fa
Jonathan Oliveira <jonathan.jb.oliveira@gmail.com>
2022-12-21 18:24:31 -0300

    feat: cadastro dedução em branco falsificação

--- src/deducao.py
+++ src/deducao.py
@@ -1,4 +1,14 @@
 class Deducao:
     def __init__(self, descricao: str, valor: int) -> None:
+        raise DescricaoEmBrancoException(descricao)
         self.descricao = descricao
         self.valor = valor
+
+
+class DescricaoEmBrancoException(Exception):
+    def __init__(self, descricao: str) -> None:
+        self.descricao = descricao
+        super().__init__(f"Descrição em branco: {descricao}")
+
+    def __str__(self) -> str:
+        return f"Descrição em branco: {self.descricao}"
