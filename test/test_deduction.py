085511457264d9fdaddcc4dbaebba2993f593a53
Jonathan Oliveira <jonathan.jb.oliveira@gmail.com>
2022-12-21 18:05:34 -0300

    feat: cadastro dedução deuplição falha

--- test/test_deduction.py
+++ test/test_deduction.py
@@ -6,3 +6,8 @@ class TestDecucoes:
         deducao = Deducao(descricao="Previdencia privada", valor=100)
         assert deducao.valor == 100
         assert deducao.descricao == "Previdencia privada"
+
+    def test_cadastro2(self):
+        deducao = Deducao(descricao="Funpresp", valor=800)
+        assert deducao.valor == 800
+        assert deducao.descricao == "Funpresp"
