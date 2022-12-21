ca92701c4ec458b0217d619f821a2ea5fbdf0bac
Jonathan Oliveira <jonathan.jb.oliveira@gmail.com>
2022-12-21 18:24:31 -0300

    feat: cadastro dedução em branco falsificação

--- test/test_deduction.py
+++ test/test_deduction.py
@@ -1,18 +1,35 @@
-from deducao import Deducao
+from deducao import Deducao, DescricaoEmBrancoException
+
+import pytest
 
 
 class TestDecucoes:
-    def test_cadastro(self):
-        deducao = Deducao(descricao="Previdencia privada", valor=100)
-        assert deducao.valor == 100
-        assert deducao.descricao == "Previdencia privada"
+    @pytest.mark.parametrize(
+        "test_input, expected",
+        [
+            (
+                {"descricao": "Previdencia privada", "valor": 100},
+                {"descricao": "Previdencia privada", "valor": 100},
+            ),
+            (
+                {"descricao": "Funpresp", "valor": 800},
+                {"descricao": "Funpresp", "valor": 800},
+            ),
+            (
+                {"descricao": "Previdencia privada", "valor": 1000},
+                {"descricao": "Previdencia privada", "valor": 1000},
+            ),
+            # pytest.param("6*9", 42, marks=pytest.mark.xfail),
+        ],
+    )
+    def test_cadastro(self, test_input, expected):
+        deducao = Deducao(
+            descricao=test_input.get("descricao"),
+            valor=test_input.get("valor"),
+        )
+        assert deducao.valor == expected.get("valor")
+        assert deducao.descricao == expected.get("descricao")
 
-    def test_cadastro2(self):
-        deducao = Deducao(descricao="Funpresp", valor=800)
-        assert deducao.valor == 800
-        assert deducao.descricao == "Funpresp"
-
-    def test_cadastro3(self):
-        deducao = Deducao(descricao="Previdencia privada", valor=1000)
-        assert deducao.valor == 1000
-        assert deducao.descricao == "Previdencia privada"
+    def test_descricao_em_branco(self):
+        with pytest.raises(DescricaoEmBrancoException):
+            Deducao(descricao="", valor=100)
