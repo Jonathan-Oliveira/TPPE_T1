3886301c19a008df068012397ee68e6b7a314fff
linktocaio <caio@wgo.com.br>
2022-12-21 19:42:09 -0300

    feat: cadastro dependente triangulação refatoracao

--- test/test_dependente.py
+++ test/test_dependente.py
@@ -4,17 +4,28 @@ import pytest
 
 class TestDependentes:
 
-    def test_cadastro(self, nome = "caio", dataNascimento = "19/10/1999"):
-        dependente = Dependente(nome, dataNascimento)
-        assert dependente.nome == "caio"
-        assert dependente.dataNascimento == "19/10/1999"
+    @pytest.mark.parametrize(
+        "test_input, expected",
+        [
+            (
+                {"nome": "Pessoa", "dataNascimento": "19/10/1999"},
+                {"nome": "Pessoa", "dataNascimento": "19/10/1999"},
+            ),
+            (
+                {"nome": "alguem", "dataNascimento": "19/10/2000"},
+                {"nome": "alguem", "dataNascimento": "19/10/2000"},
+            ),
+            (
+                {"nome": "outro", "dataNascimento": "19/10/2042"},
+                {"nome": "outro", "dataNascimento": "19/10/2042"},
+            ),
+        ],
+    )
 
-    def test_cadastro2(self, nome = "alguem", dataNascimento = "19/10/2000"):
-        dependente = Dependente(nome, dataNascimento)
-        assert dependente.nome == "alguem"
-        assert dependente.dataNascimento == "19/10/2000"
-
-    def test_cadastro2(self, nome = "outro", dataNascimento = "19/10/2042"):
-        dependente = Dependente(nome, dataNascimento)
-        assert dependente.nome == "outro"
-        assert dependente.dataNascimento == "19/10/2042"
+    def test_cadastro(self, test_input, expected):
+        dependente = Dependente(
+            nome=test_input.get("nome"),
+            dataNascimento=test_input.get("dataNascimento")
+        )
+        assert dependente.nome == expected.get("nome")
+        assert dependente.dataNascimento == expected.get("dataNascimento")
