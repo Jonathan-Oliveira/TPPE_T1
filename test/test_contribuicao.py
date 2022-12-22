f6e09f88ebe6216e865b4be2b1b4d66f02b8099c
mibasFerraz <lucasllff@gmail.com>
2022-12-21 21:31:06 -0300

    feat: cadastro de contribuicao triangulacao

--- test/test_contribuicao.py
+++ test/test_contribuicao.py
@@ -15,6 +15,10 @@ class TestContribuicao:
                 {"descricao": "Auxílio-alimentacao", "valor": 800},
                 {"descricao": "Auxílio-alimentacao", "valor": 800},
             ),
+            (
+                {"descricao": "Auxílio-Transporte", "valor": 1000},
+                {"descricao": "Auxílio-Transporte", "valor": 1000},
+            ),
         ],
     )
     def test_contribuicao(self, test_input, expected):
