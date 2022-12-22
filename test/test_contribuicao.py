6d90f23cfb0a1cbcfcf3c7fb1dbf43e71cdd6b53
mibasFerraz <lucasllff@gmail.com>
2022-12-21 21:27:53 -0300

    feat: criacao do arquivo de teste

---
+++ test/test_contribuicao.py
@@ -0,0 +1,21 @@
+from contribuicao import Contribuicao
+
+import pytest
+
+class TestContribuicao:
+    @pytest.mark.parametrize(
+        "test_input, expected",
+        [
+            (
+                {"descricao": "Aposentadoria", "valor": 100},
+                {"descricao": "Aposentadoria", "valor": 100},
+            ),
+            
+        ],
+    )
+    def test_contribuicao(self, test_input, expected):
+        contribuicao = Contribuicao(
+            descricao=test_input.get("descricao"),
+            valor=test_input.get("valor"),
+        ) 
+        
