60194fa8b830359c9720d50192cf5fdea66890a3
julisous <julia.farias2000@hotmail.com>
2023-02-08 23:28:01 -0300

    Adiçao do topico de simplicidade no codigo e finalizacao do t3 o-authored-by: Caio Martins <caio@wgo.com.br> Co-authored-by:  Jonathan Oliveira <jonathan.jb.oliveira@gmail.com> Co-authored-by: Julia Sousa <julia.farias2000@hotmail.com> Co-authored-by: Lucas Ferraz <lucasllff@gmail.com> Co-authored-by: Wellington Jonathan <wellpriz@gmail.com>

--- src/deducao.py
+++ src/deducao.py
@@ -1,29 +1,29 @@
 from typing import Any
 
 
-class Deducao:
-    def __init__(self, descricao: str, valor: int | float) -> None:
-        if not descricao or not descricao.strip():
-            raise DescricaoEmBrancoException(descricao)
-        if type(valor) not in [float, int] or not valor or valor < 0:
-            raise ValorDeducaoInvalidoException(valor)
-        self.descricao = descricao
-        self.valor = valor
+class Pensao:
+    def __init__(self, valor_pensao: int ) -> None:
+        if not valor_pensao:
+            raise ValorPensaoEmBrancoExcecao(valor_pensao)
+        if type(valor_pensao) not in [float, int] or valor_pensao < 0:
+            raise ValorPensaoInvalidaExcecao(valor_pensao)
+        self.valor_pensao = valor_pensao
 
 
-class ValorDeducaoInvalidoException(Exception):
-    def __init__(self, valor: Any) -> None:
-        self.valor = valor
-        super().__init__("Valor inválido: -100")
-
-    def __str__(self) -> str:
-        return f"Valor inválido: {self.valor}"
-
-
-class DescricaoEmBrancoException(Exception):
-    def __init__(self, descricao: Any) -> None:
-        self.descricao = descricao
-        super().__init__(f"Descrição em branco: {descricao}")
+class ValorPensaoInvalidaExcecao(Exception):
+    def __init__(self, valor_pensao: Any) -> None:
+        self.valor_pensao = valor_pensao
+        super().__init__(f"Valor inválido: {self.valor_pensao}")
 
     def __str__(self) -> str:
-        return f"Descrição em branco: {self.descricao}"
+        return f"Valor inválido: {self.valor_pensao}"
+
+
+class ValorPensaoEmBrancoExcecao(Exception):
+    def __init__(self, valor_pensao: Any) -> None:
+        self.valor_pensao = valor_pensao
+        super().__init__(f"Valor em branco: {valor_pensao}")
+
+    def __str__(self) -> str:
+        return f"Valor em branco: {self.valor_pensao}"
+
--- src/pensao.py
+++ src/pensao.py
@@ -2,15 +2,15 @@ from typing import Any
 
 
 class Pensao:
-    def __init__(self, valor_pensao: int | float) -> None:
+    def __init__(self, valor_pensao: float) -> None:
         if not valor_pensao:
-            raise ValorPensaoEmBrancoException(valor_pensao)
+            raise ValorPensaoEmBrancoExcecao(valor_pensao)
         if type(valor_pensao) not in [float, int] or valor_pensao < 0:
-            raise ValorPensaoInvalidaException(valor_pensao)
+            raise ValorPensaoInvalidaExcecao(valor_pensao)
         self.valor_pensao = valor_pensao
 
 
-class ValorPensaoInvalidaException(Exception):
+class ValorPensaoInvalidaExcecao(Exception):
     def __init__(self, valor_pensao: Any) -> None:
         self.valor_pensao = valor_pensao
         super().__init__(f"Valor inválido: {self.valor_pensao}")
@@ -19,7 +19,7 @@ class ValorPensaoInvalidaException(Exception):
         return f"Valor inválido: {self.valor_pensao}"
 
 
-class ValorPensaoEmBrancoException(Exception):
+class ValorPensaoEmBrancoExcecao(Exception):
     def __init__(self, valor_pensao: Any) -> None:
         self.valor_pensao = valor_pensao
         super().__init__(f"Valor em branco: {valor_pensao}")
